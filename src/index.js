dd6d181b8ed6c323da3c8dc0cccd5dff084839c7
IlyaOrluk <misfits120@mail.ru>
2020-02-02 23:50:31 +0300

    refactoring game

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { Engine, World, Body, Bodies, Constraint, Render, Mouse, MouseConstraint } from 'matter-js'
+import { Engine, World, Body, Bodies, Constraint, Render, Mouse, MouseConstraint, Composites, Composite, Query, Events } from 'matter-js'
 
 // create an engine
 let engine = Engine.create(),
@@ -10,7 +10,7 @@ let engine = Engine.create(),
         options: {
             width: 1000,
             height: 600,
-            background: '#0f0f12',
+            background: 'https://cdn.dribbble.com/users/168664/screenshots/3500505/game-background-sprite.jpg',
             // showBroadphase: true,
             // showAxes: true,
             // showCollisions: true,
@@ -21,13 +21,7 @@ let engine = Engine.create(),
 })
 
 // create two boxes and a ground
-let boxA = Bodies.rectangle(400, 120, 100, 100, {
-    isSleeping: true,
-    frictionAir: 0.0005,
-    density: 0.05
-}),
-    boxX = Bodies.rectangle(660, 120, 40, 40),
-    boxB = Bodies.rectangle(170, 500, 200, 50, {
+   let boxB = Bodies.rectangle(170, 500, 200, 50, {
         isStatic: true,
         chamfer: 10,
         slop: 1,
@@ -44,104 +38,57 @@ let groundB = Bodies.rectangle(400, 610, 1110, 60, { isStatic: true }),
     groundL = Bodies.rectangle(980, 0, 60, 1210, { isStatic: true }),
     groundR = Bodies.rectangle(0, 0, 60, 1210, { isStatic: true })
 
-let buble1 = Bodies.circle(400, 100, 25, {
-    render: {
-         fillStyle: 'red',
-         strokeStyle: 'blue',
-         lineWidth: 3
-    },
-    frictionAir: 0.0006,
-    restitution: 0.3
-})
-const ballConfig = {
-    render: {
-        strokeStyle: 'red',
-        sprite: {
-            texture: 'https://avatars.mds.yandex.net/get-pdb/25978/29291878-25be-4840-bc5b-6547a99970bf/s375',
-            xScale: .12,
-            yScale: .12,
-        }
-    },
-    frictionAir: 0.005,
-    restitution: 1,
-    density: 0.003
-}
-let ball1 = Bodies.circle(80, 400, 20, ballConfig),
-    ball2 = Bodies.circle(120, 400, 20, ballConfig),
-    ball3 = Bodies.circle(120, 360, 20, ballConfig),
-    ball4 = Bodies.circle(160, 320, 20, ballConfig),
-    ball5 = Bodies.circle(180, 320, 20, ballConfig),
-    ball6 = Bodies.circle(140, 320, 20, ballConfig)
-
-    World.add(world, [ball1, ball2, ball3, ball4, ball5, ball6]);
-
-
-
-
-
-
-    // var body = Bodies.polygon(150, 200, 5, 30);
-
-    // var constraint = Constraint.create({
-    //     pointA: { x: 150, y: 100 },
-    //     bodyB: body,
-    //     pointB: { x: -10, y: -10 }
-    // });
-
-    // World.add(world, [body, constraint]);
-
-    // add soft global constraint
-    // var body = Bodies.polygon(280, 100, 3, 30);
-
-    // var constraint = Constraint.create({
-    //     pointA: { x: 280, y: 120 },
-    //     bodyB: body,
-    //     pointB: { x: -10, y: -7 },
-    //     stiffness: 0.001
-    // });
-
-    // World.add(world, [body, constraint]);
-
-    // add damped soft global constraint
-    // var body = Bodies.polygon(400, 100, 4, 30);
-
-    // var constraint = Constraint.create({
-    //     pointA: { x: 400, y: 120 },
-    //     bodyB: body,
-    //     pointB: { x: -10, y: -10 },
-    //     stiffness: 0.001,
-    //     damping: 0
-    // });
-
-    // World.add(world, [body, constraint]);
-
-
-
-
-    let catapult = Bodies.rectangle(240, 400, 400, 20, {
-        angle: 9
-    })
-    var constraint = Constraint.create({
-        pointA: { x: 240, y: 400 },
-        bodyB: catapult,
-        length: 0,
-    });
-
-    World.add(world, [catapult, constraint]);
-
-    const wallOpt = {
-        render: {
-            fillStyle: 'yellow',
-            lineWidth: 4
-       },
-       density: 0.0003
+class Ball {
+    constructor(x, y, radius){
+        this.x = x,
+        this.y = y,
+        this.radius = radius
+        this.options = {
+            render: {
+                sprite: {
+                    texture: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Basketball_Clipart.svg/1035px-Basketball_Clipart.svg.png',
+                    xScale: .037,
+                    yScale: .037,
+                }
+            },
+            frictionAir: 0.005,
+            restitution: .9,
+            density: 0.003
+        },
+        this.body = Bodies.circle(this.x, this.y, this.radius, this.options)    
     }
-    let wall1 = Bodies.rectangle(860, 500, 40, 200, wallOpt),
-        wall2 = Bodies.rectangle(760, 500, 40, 200, wallOpt),
-        wall3 = Bodies.rectangle(810, 300, 160, 30, wallOpt),
-        wall4 = Bodies.rectangle(810, 100, 30, 150, wallOpt)
+    show() {
+        World.add(world, [this.body]);
+    } 
+}
 
-    World.add(world, [wall1, wall2, wall3, wall4]);
+let balls = []
+
+
+    let wall = Bodies.rectangle(840, 145, 15, 150, {
+        fillStyle: '#ffffff',
+        isStatic: true,
+    }),
+        ring = Bodies.rectangle(749, 197, 92, 10, {
+            isStatic: true,
+            isSensor: true,
+            chamfer: 5,
+        }),
+        subRing = Bodies.rectangle(820, 197, 50, 5, {
+            isStatic: true,
+        }),
+        support = Bodies.rectangle(899, 197, 150, 15, {
+            isStatic: true,
+            angle: 98
+        }),
+        restriction1 = Bodies.circle(789, 195, 2, {
+            isStatic: true
+        }),
+        restriction2 = Bodies.circle(709, 195, 2, {
+            isStatic: true
+        })
+
+    World.add(world, [wall, ring, subRing, support, restriction1, restriction2]);
     
 let mouse = Mouse.create(render.canvas),
     mouseConstraint = MouseConstraint.create(engine, {
@@ -153,14 +100,122 @@ let mouse = Mouse.create(render.canvas),
             }
         }
     });
+    let mouseX, mouseY
+    window.addEventListener('mousemove', () => {
+        mouseX = mouse.position.x
+        mouseY = mouse.position.y
 
-World.add(engine.world, mouseConstraint);
+    })
+    window.addEventListener('keydown', e => {
+        console.log(e.keyCode, mouseX, mouseY)
+        if(e.keyCode === 32){
+            balls.push(new Ball(300, 400, 20))
+            balls[balls.length-1].show()
+            Body.setVelocity( balls[balls.length-1].body, {x: -((mouse.position.x-300)/10), y: -((mouse.position.y-400)/10)})
+            Body.setAngularVelocity( balls[balls.length-1].body, -Math.PI/6)
+        }
+    })
+
+    Events.on(render, 'afterRender', function() {
+        var mouse = mouseConstraint.mouse,
+            context = render.context,
+            bodies = Composite.allBodies(engine.world),
+            startPoint = { x: 300, y: 400 },
+            endPoint = mouse.position;
+        let distance = Math.sqrt(Math.pow((mouse.position.x - startPoint.x),2)+Math.pow((mouse.position.y - startPoint.y),2))
+        var collisions = Query.ray(bodies, startPoint, endPoint);
+        console.log(mouse.position.x-300, mouse.position.y-400)
+        Render.startViewTransform(render);
+
+        context.beginPath();
+        context.moveTo(startPoint.x, startPoint.y);
+        context.lineTo(endPoint.x, endPoint.y);
+
+        context.fillText(Math.floor(distance), endPoint.x, endPoint.y)
+        if (collisions.length > 0) {
+            context.strokeStyle = '#FF0000';
+        } else {
+            context.strokeStyle = '#00FF00';
+        }
+        context.lineWidth = 2;
+        context.stroke();
+
+        for (var i = 0; i < collisions.length; i++) {
+            var collision = collisions[i];
+            context.rect(collision.bodyA.position.x - 4.5, collision.bodyA.position.y - 4.5, 8, 8);
+        }
+
+        context.fillStyle = 'rgba(255,165,0,0.7)';
+        context.fill();
+
+        Render.endViewTransform(render);
+    }); 
+
+World.add(world, mouseConstraint);
+
+
+let group = Body.nextGroup(true),
+    particleOptions = { friction: 0.00001, density: 0.0003, collisionFilter: { group: group }, render: { visible: false }},
+    constraintOptions = { stiffness: 0.3 },
+    cloth = Composites.softBody(710, 190, 8, 6, 1, 1, false, 4, particleOptions, constraintOptions)
+
+World.add(world, [
+    cloth,
+    Constraint.create({ 
+        bodyA: cloth.bodies[0], 
+        pointB: {x: 709, y: 195},
+        stiffness: 1,
+        length: 0
+    }),
+    Constraint.create({ 
+        bodyA: cloth.bodies[1], 
+        pointB: {x: 720, y: 195},
+        stiffness: 1,
+        length: 8
+    }),
+    Constraint.create({ 
+        bodyA: cloth.bodies[2], 
+        pointB: {x: 730, y: 195},
+        stiffness: 1,
+        length: 14
+    }),
+    Constraint.create({ 
+        bodyA: cloth.bodies[3], 
+        pointB: {x: 741, y: 195},
+        stiffness: 1,
+        length: 16
+    }),
+    Constraint.create({ 
+        bodyA: cloth.bodies[4], 
+        pointB: {x: 755, y: 195},
+        stiffness: 1,
+        length: 16
+    }),
+    Constraint.create({ 
+        bodyA: cloth.bodies[5], 
+        pointB: {x: 770, y: 195},
+        stiffness: 1,
+        length: 14
+    }),
+    Constraint.create({ 
+        bodyA: cloth.bodies[6], 
+        pointB: {x: 780, y: 195},
+        stiffness: 1,
+        length: 8
+    }),
+    Constraint.create({ 
+        bodyA: cloth.bodies[7], 
+        pointB: {x: 789, y: 195},
+        stiffness: 1,
+        length: 0
+    }),
+]);
 
 // keep the mouse in sync with rendering
 render.mouse = mouse;
 
 // add all of the bodies to the world
-World.add(engine.world, [groundB, groundT, groundL, groundR, boxA, boxB])
+World.add(engine.world, [groundB, groundT, groundL, groundR])
 // buble1, buble2,
 // run the engine
 Engine.run(engine)
